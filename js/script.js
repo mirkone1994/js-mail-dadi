87bc185fe4b63a55adf48d708fa6053d94628b12
mirkone1994 <mirkochiappori@gmail.com>
2021-07-05 10:33:26 +0200

    correction mail

--- js/script.js
+++ js/script.js
@@ -3,9 +3,16 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.*/
 var email = prompt("Inserisci la tua email per accedere");
-var listEmail = ["pippo@mail.it"];
+var listEmail = ["pippo@mail.it", "pluto@mail.it", "paperino@mail.it"];
 var mail = document.getElementById ("mail");
 console.table (listEmail);
-if (email == listEmail[0]) {
-    mail.innerHTML = "Benvenuto";
-} else alert("Questa email non è registrata nei nostri sistemi, accesso negato");
+var isAllowed = false;
+for (var i = 0; i < listEmail.length; i++) {
+    if (email === listEmail[i]) {
+        isAllowed = true;
+        mail.innerHTML = "Benvenuto";
+    } 
+}
+if (!isAllowed) {
+    alert("Questa email non è registrata nei nostri sistemi, accesso negato");
+}
